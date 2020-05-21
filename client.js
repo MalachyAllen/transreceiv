b5a21d8caa08abde9a7a274dbee264c8b78d767d
Fayti1703 <Fayti1703@loop.de>
2020-05-21 09:10:36 +0200

    Refactor client.js to use Promises / async+await
    Why does nobody use 3-year-old features over comparatively ANCIENT
    patterns in JavaScript again?

--- client.js
+++ client.js
@@ -8,44 +8,67 @@ const NoIPError = 'An IP address must be entered.';
 const InvalidIPError = 'Invalid IP address entered.';
 const NoMessageContentError = 'A message must be entered.';
 
+const RLInputClosedError = 'Readline Input was closed.';
+
 const RLINPUT = READLINE.createInterface({
     input: PROCESS.stdin,
     output: PROCESS.stdout
 });
 
-RLINPUT.question('IP to connect to: ', (host) => {
-    if (host == null) {
-        throw NoIPError;
-    }
+function RLQUESTION(rl, question) {
+	return new Promise((resolve, reject) => {
+		function closeListener() {
+			reject(RLInputClosedError);
+		}
+		rl.once('close', closeListener);
+		rl.question(question, (answer) => {
+			rl.off('close', closeListener);
+			resolve(answer);
+		});
+	})
+}
 
-    if (NET.isIP(host) != 0 || host == 'localhost') {
-        console.log('IP address correct. IP is ' + host);
-        const CLIENT = new NET.Socket();
-        CLIENT.connect(PORT, host, function() {
-            console.log('CONNECTING TO: ' + host + ':' + PORT + '...');
-        });
-        CLIENT.on('connect', function() {
-            console.log('CONNECTION ESTABLISHED AT: ' + host + ':' + PORT);
-        });
-        CLIENT.on('ready', function() {
-            RLINPUT.question('Enter the message you want to send: ', (data) => {
-                if (data == null) {
-                    throw NoMessageContentError;
-                }
-                else {
-                    console.log('SENDING DATA: ' + data);
-                    CLIENT.write(data);
-                }
-            });
-        });
-        CLIENT.on('close', function() {
-            CLIENT.destroy();
-            console.log('Connection closed.');
-        });
-    }
-    else {
-        throw InvalidIPError;
-    }
+function UNTILEVENT(emitter, event) {
+	return new Promise((resolve, reject) => {
+		emitter.once(event, (...a) => resolve(...a))
+	});
+}
 
-    RLINPUT.close();
-});
+;(async () => {
+	const host = await RLQUESTION(RLINPUT, 'IP to connect to: ');
+	if (host == null) {
+		RLINPUT.close();
+		throw NoIPError;
+	}
+	console.log('Received remote: ' + host);
+
+	const CLIENT = new NET.Socket();
+	CLIENT.connect(PORT, host, function() {
+		console.log('CONNECTING TO: ' + host + ':' + PORT + '...');
+	});
+	CLIENT.on('connect', function() {
+		console.log('CONNECTION ESTABLISHED AT: ' + host + ':' + PORT);
+	});
+
+	CLIENT.on('close', function() {
+		CLIENT.destroy();
+		RLINPUT.close();
+		console.log('Connection closed.');
+	});
+
+	await UNTILEVENT(CLIENT, 'ready');
+
+	try {
+		const data = await RLQUESTION(RLINPUT, 'Enter the message you want to send: ');
+		if (data == null) {
+			throw NoMessageContentError;
+		} else {
+			CLIENT.write(data, 'utf-8');
+		}
+	} catch(e) {
+		console.log(e === RLInputClosedError ? '<close signal>' : '');
+		CLIENT.destroy();
+		if(e !== RLInputClosedError)
+			throw e;
+	}
+})()
